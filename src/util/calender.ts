badc3d91a840293c398607f8fbdd327dc961b9ab
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-08-25 12:30:57 +0530

    fy calculator

--- src/util/calender.ts
+++ src/util/calender.ts
@@ -1,5 +1,15 @@
 export function getAllFY() {
   const fy = [];
+  let currentYear = getcurrentFY();
+
+  for (let year = 2020; year <= currentYear; year++) {
+    fy.push(`FY ${year}-${year + 1}`);
+  }
+
+  return fy;
+}
+
+export function getcurrentFY() {
   const date = new Date();
   let currentYear = date.getFullYear();
   const currentMonth = date.getMonth() + 1;
@@ -7,10 +17,10 @@ export function getAllFY() {
   if (currentMonth < 4) {
     currentYear--;
   }
+  return currentYear;
+}
 
-  for (let year = 2020; year <= currentYear; year++) {
-    fy.push(`FY ${year}-${year + 1}`);
-  }
-
-  return fy;
+export function formattedFY() {
+  const year = getcurrentFY();
+  return `FY ${year}-${year + 1}`;
 }
